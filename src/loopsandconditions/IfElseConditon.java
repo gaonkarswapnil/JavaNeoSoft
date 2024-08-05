70c0ccc00fcd59b0a05ef5e60cdc5f6737bff5cf
gaonkarswapnil <gaonkarswapnil1@gmail.com>
2024-08-05 10:51:57 +0530

    If Else Condition

---
+++ src/loopsandconditions/IfElseConditon.java
@@ -0,0 +1,13 @@
+package loopsandconditions;
+
+public class IfElseConditon {
+    public static void main(String[] args) {
+        int x = 10, y = 12;
+        if (x + y < 10){
+            System.out.println("x+y is less than 10");
+        }
+        else {
+            System.out.println("x+y is greater to 10");
+        }
+    }
+}
