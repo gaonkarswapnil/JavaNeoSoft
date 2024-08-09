2ae1340b0261da3d9ff70726a57c936f75775591
gaonkarswapnil <gaonkarswapnil1@gmail.com>
2024-08-09 10:53:39 +0530

    Thread Class, Inline function and Lambda

--- src/Multithreading/ThreadClassExample.java
+++ src/Multithreading/ThreadClassExample.java
@@ -17,9 +17,25 @@ class Thread2 extends Thread {
 }
 public class ThreadClassExample {
     public static void main(String[] args) {
-        Thread1 t1 = new Thread1();
-        Thread2 t2 = new Thread2();
+        Thread t1 = new Thread1();
+        Thread t2 = new Thread2();
+        Thread t3 = new Thread(){
+            public void run() {
+                for (int i = 0; i < 20; i++) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                }
+            }
+        };
+        Thread t4 = new Thread(
+                () -> {
+                    for (int i = 0; i < 25; i++) {
+                        System.out.println(Thread.currentThread().getName() + " " + i);
+                    }
+                }
+        );
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
     }
 }
