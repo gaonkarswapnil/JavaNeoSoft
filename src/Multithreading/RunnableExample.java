3f11ac88d524639191f40fa1cfed3f6fb97aa98d
gaonkarswapnil <gaonkarswapnil1@gmail.com>
2024-08-09 10:46:53 +0530

    Runnable Interface, Inline function and Lambda

--- src/Multithreading/RunnableExample.java
+++ src/Multithreading/RunnableExample.java
@@ -14,7 +14,7 @@ class ThreadTwo implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("Thread 2 "+i);
         }
     }
@@ -24,7 +24,22 @@ public class RunnableExample {
     public static void main(String[] args) {
         Thread t1 = new Thread(new ThreadOne());
         Thread t2 = new Thread(new ThreadTwo());
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 20; i++) {
+                    System.out.println("Thread 3 "+i);
+                }
+            }
+        });
+
+        Thread t4 = new Thread(() -> {
+            for (int i = 0; i < 25; i++) {
+                System.out.println("Thread 4 "+i);
+            }
+        });
         t1.start();
         t2.start();
+        t3.start();
     }
 }
